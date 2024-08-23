87740ba1b9dde10ff2ff7dfbc73f358106397411
kseniyazhuravleva <ksyunya.lobova@bk.ru>
2024-08-23 23:49:02 +0300

    first blue screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -19,15 +22,24 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(!gladLoadGL()){
+        std::cout << "ooops" << std::endl;
+        return -1;
+    }
+
+    glClearColor(0.3,0,1,1);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
+        
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
+        
         /* Poll for and process events */
         glfwPollEvents();
     }
